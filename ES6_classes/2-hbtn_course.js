e4d944e1643dba6c04dcb2aec8ee5ed37a48071f
Iraduhaye Bukuru Paterne <116680462+IraduhayeBukuruPaterne1@users.noreply.github.com>
2024-11-09 02:11:38 +0000

    paterne

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -1,4 +1,3 @@
-
 export default class HolbertonCourse {
   constructor(name, length, students) {
     this.name = name;
@@ -39,4 +38,3 @@ export default class HolbertonCourse {
     this._students = newStudents;
   }
 }
-
